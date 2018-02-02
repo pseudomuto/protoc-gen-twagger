145e39dbb690202bc449b60d234d5f6448420ee0
David Muto (pseudomuto) <david.muto@gmail.com>
2018-02-01 23:15:33 -0500

    Add a couple more tests to the generated output

--- internal/plugin_test.go
+++ internal/plugin_test.go
@@ -3,10 +3,12 @@ package internal_test
 import (
 	"github.com/stretchr/testify/suite"
 
+	"encoding/json"
 	"testing"
 
 	"github.com/pseudomuto/protoc-gen-twagger/internal"
 	"github.com/pseudomuto/protoc-gen-twagger/internal/utils"
+	"github.com/pseudomuto/protoc-gen-twagger/options"
 )
 
 type PluginTest struct {
@@ -26,4 +28,21 @@ func (assert *PluginTest) TestGenerate() {
 	file := resp.GetFile()[0]
 	assert.Equal("swagger.json", file.GetName())
 	assert.Contains(file.GetContent(), "{\n  \"openapi\": \"3.0\",")
+}
+
+func (assert *PluginTest) TestGeneratedContent() {
+	req, _ := utils.LoadCodeGenRequest()
+	resp, err := internal.NewPlugin(req).Generate()
+	assert.NoError(err)
+
+	var api options.OpenAPI
+	assert.NoError(json.Unmarshal([]byte(resp.GetFile()[0].GetContent()), &api))
+
+	assert.Equal("3.0", api.Openapi)
+	assert.Equal("Todo API", api.Info.Title)
+	assert.Equal("Apache 2.0", api.Info.License.Name)
+
+	assert.Len(api.Servers, 1)
+	assert.Len(api.Tags, 2)
+	assert.Len(api.Paths, 4)
 }
